33911f1d69ee02644d785cb1a31619904dc2fdf6
JohnSovero <u202115065@upc.edu.pe>
2023-10-01 10:56:56 -0500

    Notebooks to learn image processing

---
+++ src/Ejemplo - Estacionamientos/1. Estacionamiento PKL/main.py
@@ -0,0 +1,18 @@
+import cv2
+import pickle
+
+img = cv2.imread('estacionamiento.png')
+
+espacios = []
+
+for x in range(2):
+    espacio = cv2.selectROI('espacio', img, False)
+    cv2.destroyWindow('espacio')
+    espacios.append(espacio)
+
+    for x, y, w, h in espacios:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255,0,0), 2)
+
+print("Espacios", espacios)
+with open('espacios.pkl','wb') as file:
+    pickle.dump(espacios, file)
